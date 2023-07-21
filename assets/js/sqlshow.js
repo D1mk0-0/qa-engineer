3e696baef0a9ec245d34ca83759ffb46511f4d07
Dmitriy <D1mk0Leb@yandex.ru>
2023-07-21 16:05:18 +0300

    qa engeneer 0.02

--- assets/js/sqlshow.js
+++ assets/js/sqlshow.js
@@ -1,6 +1,10 @@
 var captionLength = 0;
 
-var caption = "CREATE TABLE employee (\
+var caption = '';
+
+var ready = '';
+
+var create = "CREATE TABLE employee (\
     <br \/> id BIGSERIAL NOT NULL PRIMARY KEY,\
     <br \/> first_name VARCHAR(50) NOT NULL,\
     <br \/> last_name VARCHAR(50) NOT NULL,\
@@ -10,50 +14,68 @@ var caption = "CREATE TABLE employee (\
     <br \/> country_of_birth VARCHAR (50) NOT NULL\
     <br \/>)\;";
 
+var insert = "INSERT INTO employee (\
+    <br \/> first_name,\
+    <br \/> last_name,\
+    <br \/> gender,\
+    <br \/> email,\
+    <br \/> date_of_birth,\
+    <br \/> country_of_birth\
+    <br \/> )\
+    <br \/> VALUES ('John', 'Doe', 'MALE', 'jd@mail.com', '2000-01-01', 'United Kingdom');";
+
+var select = "SELECT country_of_birth, COUNT(*) > 10\
+    <br \/> FROM employee GROUP BY country_of_birth\
+    <br \/> HAVING COUNT(*) > 10\
+    <br \/> ORDER BY country_of_birth DESC;";
 
 $(document).ready(function() {
     setInterval ('cursorAnimation()', 600);
     captionEl = $('#caption');
-    
-    $('#test-typing').click(function(){
+
+    $('#test-create').click(function(){
+        captionLength = 0;
+        ready = create;
         testTypingEffect();
     });
 
-    $('#test-erasing').click(function(){
-        testErasingEffect();
+    $('#test-insert').click(function(){
+        captionLength = 0;
+        ready = insert;
+        testTypingEffect();
     });
+
+    $('#test-select').click(function(){
+        captionLength = 0;
+        ready = select;
+        testTypingEffect();
+    });
+
+    
+
 });
 
 function testTypingEffect() {
-    caption;
-    type();
+    captionEl.html(caption.substr(0, captionLength--));
+    if(captionLength >= 0) {
+        erase();
+    } else {
+        caption = ready;
+        type();
+    }
 }
 
 function type() {
     captionEl.html(caption.substr(0, captionLength++));
     if(captionLength < caption.length+1) {
         setTimeout('type()', 50);
-    } else {
-        captionLength = 0;
-        caption = '';
-    }
-}
-
-function testErasingEffect() {
-    caption = captionEl.html();
-    captionLength = caption.length;
-    if (captionLength>0) {
-        erase();
-    } else {
-        $('#caption').html("You didn't write anything to erase, but that's ok!");
-        setTimeout('testErasingEffect()', 1000);
-    }
+    };
 }
 
 function erase() {
     captionEl.html(caption.substr(0, captionLength--));
     if(captionLength >= 0) {
-        setTimeout('erase()', 50);
+        setTimeout('erase()', 5);
     } else {
         captionLength = 0;
         caption = '';
