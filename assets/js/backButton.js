0ff822397378234a32044050c63e6551b01e6707
Dmitriy <D1mk0Leb@yandex.ru>
2023-09-03 19:29:24 +0300

    qa-engineer 1.1

---
+++ assets/js/backButton.js
@@ -0,0 +1,9 @@
+$(".modal").on("shown.bs.modal", function()  {
+    var urlReplace = "#" + $(this).attr('id'); 
+    history.pushState(null, null, urlReplace); 
+  });
+
+  // This code gets executed when the back button is clicked, hide any/open modals.
+  $(window).on('popstate', function() { 
+    $(".modal").modal('hide');
+  });
