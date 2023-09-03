b4ad8b2a0351684a339fe3cbdee01b162533db56
Dmitriy <D1mk0Leb@yandex.ru>
2023-09-03 20:54:21 +0300

    qa-engineer 1.2

--- assets/js/backButton.js
+++ assets/js/backButton.js
@@ -1,13 +1,3 @@
-//$(".modal").on("shown.bs.modal", function()  {
-//    var urlReplace = "#" + $(this).attr('id'); 
-//    history.pushState(null, null, urlReplace); 
-//  });
-//
-//  // This code gets executed when the back button is clicked, hide any/open modals.
-//  $(window).on('popstate', function() { 
-//    $(".modal").modal('hide');
-//  });
-
   document.addEventListener('backbutton',() => {
     // Проверяем, есть ли на модальном окне .visible
     // Убираем .visible с модального окнаа
