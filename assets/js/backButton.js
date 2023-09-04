44580d397674c72aed21ef6b4467c64384c4e085
Dmitriy <D1mk0Leb@yandex.ru>
2023-09-04 10:17:11 +0300

    qa-engineer 1.2.1

--- assets/js/backButton.js
+++ assets/js/backButton.js
@@ -1,13 +1,9 @@
-  document.addEventListener('backbutton',() => {
-    // Проверяем, есть ли на модальном окне .visible
-    // Убираем .visible с модального окнаа
-    $(".modal").on("shown.bs.modal", function()  {
-            var urlReplace = "#" + $(this).attr('id'); 
-            history.pushState(null, null, urlReplace); 
-          });
-        
-          // This code gets executed when the back button is clicked, hide any/open modals.
-          $(window).on('popstate', function() { 
-            $(".modal").modal('hide');
-          });
-});
+$(".modal").on("shown.bs.modal", function()  {
+   var urlReplace = "#" + $(this).attr('id'); 
+   history.pushState(null, null, urlReplace); 
+ });
+
+ // This code gets executed when the back button is clicked, hide any/open modals.
+ $(window).on('popstate', function() { 
+   $(".modal").modal('hide');
+ });
